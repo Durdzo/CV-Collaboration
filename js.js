642ac4039d90bfabbaf10d2a966c9cedbde9fbbb
Barmok67 <bartekmokrosz67@gmail.com>
2023-04-09 10:13:28 +0200

    first commit

---
+++ js.js
@@ -0,0 +1,10 @@
+const startBtn = document.querySelector("#start") 
+const rockBtn = document.querySelector("#rock");
+const paperBtn = document.querySelector("#paper");
+const scissorsBtn = document.querySelector("#scissors");
+
+startBtn.onclick = startGame
+rockBtn.onclick = rock
+paperBtn.onclick = paper
+scissorsBtn.onclick = scissors
+
