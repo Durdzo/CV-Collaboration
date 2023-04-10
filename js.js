4c4dfa956db6c1f7503855ae0ec2120fb2edf5bf
Mateusz Dukowicz <matti.matt45@gmail.com>
2023-04-10 20:04:04 +0200

    Papier kamień

---
+++ js.js
@@ -0,0 +1,13 @@
+const startBtn = document.querySelector("#start") 
+const rockBtn = document.querySelector("#rock");
+const paperBtn = document.querySelector("#paper");
+const scissorsBtn = document.querySelector("#scissors");
+
+startBtn.onclick = startGame
+rockBtn.onclick = rock
+paperBtn.onclick = paper
+scissorsBtn.onclick = scissors
+
+function startGame() {
+  console.log("Hello");
+}
