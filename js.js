c581ff8e8374a8074dca6a66a4098eb4191919b0
Barmok67 <bartekmokrosz67@gmail.com>
2023-04-15 13:25:55 +0200

    zxc

--- js.js
+++ js.js
@@ -13,57 +13,71 @@ element.style.display = "block";
 });
 
 
-const computerChoiceDisplay = document.getElementById('computer-choice')
-const userChoiceDisplay = document.getElementById('user-choice')
-const resultDisplay = document.getElementById('result')
-const possibleChoices = document.querySelectorAll('button')
-let userChoice
-let computerChoice
-let result
+const computerChoiceDisplay = document.getElementById('computer-choice');
+const userChoiceDisplay = document.getElementById('user-choice');
+const resultDisplay = document.getElementById('result');
+const userScoreDisplay = document.getElementById('user-score');
+const computerScoreDisplay = document.getElementById('computer-score');
+const possibleChoices = document.querySelectorAll('button');
+
+let userChoice;
+let computerChoice;
+let result;
+let userScore = 0;
+let computerScore = 0;
 
 possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
-  userChoice = e.target.id
-  userChoiceDisplay.innerHTML = userChoice
-  generateComputerChoice()
-  getResult()
-}))
+  userChoice = e.target.id;
+  userChoiceDisplay.innerHTML = userChoice;
+  generateComputerChoice();
+  getResult();
+}));
 
 function generateComputerChoice() {
-  const randomNumber = Math.floor(Math.random() * 3) + 1
+  const randomNumber = Math.floor(Math.random() * 3) + 1;
   
   if (randomNumber === 1) {
-    computerChoice = 'rock'
+    computerChoice = 'rock';
   }
   if (randomNumber === 2) {
-    computerChoice = 'scissors'
+    computerChoice = 'scissors';
   }
   if (randomNumber === 3) {
-    computerChoice = 'paper'
+    computerChoice = 'paper';
   }
-  computerChoiceDisplay.innerHTML = computerChoice
+  computerChoiceDisplay.innerHTML = computerChoice;
 }
 
 function getResult() {
   if (computerChoice === userChoice) {
-    result = 'Its a Draw!'
+    result = 'Its a Draw!';
   }
   if (computerChoice === 'rock' && userChoice === "paper") {
-    result = 'You Win!'
+    result = 'You Win!';
+    userScore++;
   }
   if (computerChoice === 'rock' && userChoice === "scissors") {
-    result = 'You Lost!'
+    result = 'You Lost!';
+    computerScore++;
   }
   if (computerChoice === 'paper' && userChoice === "scissors") {
-    result = 'You Win!'
+    result = 'You Win!';
+    userScore++;
   }
   if (computerChoice === 'paper' && userChoice === "rock") {
-    result = 'You Lose!'
+    result = 'You Lose!';
+    computerScore++;
   }
   if (computerChoice === 'scissors' && userChoice === "rock") {
-    result = 'You Win!'
+    result = 'You Win!';
+    userScore++;
   }
   if (computerChoice === 'scissors' && userChoice === "paper") {
-    result = 'You Lose!'
+    result = 'You Lose!';
+    computerScore++;
   }
-  resultDisplay.innerHTML = result
+  resultDisplay.innerHTML = result;
+  userScoreDisplay.innerHTML = userScore;
+  computerScoreDisplay.innerHTML = computerScore;
 }
+
