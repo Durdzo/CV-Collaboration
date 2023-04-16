97b137c3e3542700746711d5ec2b22c9648a666a
Mateusz Dukowicz <matti.matt45@gmail.com>
2023-04-16 19:45:26 +0200

    Przed poprawą RWD

--- js/script.js
+++ js/script.js
@@ -1,6 +1,21 @@
 const button = document.getElementById("burger-btn");
 const nav = document.getElementById("nav-box");
+const links = document.querySelectorAll(".nav-item");
+const footerYear = document.querySelector(".footer-year");
 
 button.addEventListener("click", () => {
     nav.classList.toggle("show");
+
+    links.forEach((item) => {
+        item.addEventListener("click", () => {
+            nav.classList.remove("show");
+        });
+    });
 });
+
+const handleCurrentYear = () => {
+    const year = new Date().getFullYear();
+    footerYear.innerText = year;
+};
+
+handleCurrentYear();
